3330a1388b8fcb4e36b1f3c8fd68026953576ba6
Thomas McGarry <mcgarry_tom@hotmail.com>
2018-08-08 19:24:24 +0100

    Refactored logIn code and adjusted Notifcation Center observers

--- ChatApplication2018/HomeTabBarController.swift
+++ ChatApplication2018/HomeTabBarController.swift
@@ -16,30 +16,9 @@ class HomeTabBarController: UITabBarController {
     var chatsViewController: ChatsViewController?
     var xmppController: XMPPController?
     var loggedIn = false
-    var count = 0
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "loginView" {
-            let viewController = segue.destination as! LoginViewController
-            viewController.delegate = self
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -55,13 +34,20 @@ class HomeTabBarController: UITabBarController {
         let retrievedUserName: String? = KeychainWrapper.standard.string(forKey: "userName")
         
         if retrievedPassword == nil && retrievedUserName == nil && !loggedIn {
-            //bring up log in page
+            // Segue to LoginViewController for user to login
             self.performSegue(withIdentifier: "loginView", sender: nil)
         } else if !loggedIn {
-            //login with stored credentials
+            // Otherwise, login with the credentials that are saved on the system
             autoLogIn(userJID: retrievedUserName!, userPassword: retrievedPassword!)
         }
         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "loginView" {
+            let viewController = segue.destination as! LoginViewController
+            viewController.delegate = self
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -74,23 +60,20 @@ class HomeTabBarController: UITabBarController {
     
     func autoLogIn(userJID: String, userPassword: String) {
         do {
-            //need to reconfigure so that I am not sending the host name every time
-            //May also be issues with userJID - full or with domain?
-            //Also, duplicated code
-            count = 6
-            try self.xmppController = XMPPController(userJIDString: userJID,
-                                                     password: userPassword)
-            //init(hostName: String, userJIDString: String, hostPort: UInt16 = 5222, password: String)
-            self.xmppController?.xmppStream?.addDelegate(self, delegateQueue: DispatchQueue.main)
-            self.xmppController?.connect()
-            
+            try logIn(userJID: userJID, userPassword: userPassword)
             print("Automatically logged in with saved credentials")
+            loggedIn = true
         } catch {
             //need to show an error message to the user, below is just for testing
             print("something went wrong")
         }
-        loggedIn = true
-        NotificationCenter.default.post(name: .loggedIn, object: nil)
+    }
+    
+    func logIn(userJID: String, userPassword: String) throws {
+            try self.xmppController = XMPPController(userJIDString: userJID,
+                                                     password: userPassword)
+            self.xmppController?.xmppStream?.addDelegate(self, delegateQueue: DispatchQueue.main)
+            self.xmppController?.connect()
     }
     
     @objc func logOut(notfication: NSNotification) {
@@ -102,7 +85,17 @@ class HomeTabBarController: UITabBarController {
 }
 
 extension HomeTabBarController: LoginViewControllerDelegate {
-    
+    func didTouchLogIn(sender: LoginViewController, userJID: String, userPassword: String) {
+        self.loginViewController = sender
+        do {
+            try logIn(userJID: userJID, userPassword: userPassword)
+            print("Logged in with new credentials")
+            loggedIn = true
+        } catch {
+            sender.showErrorMessage(message: "Something went wrong")
+        }
+    }
+    /*
     func didTouchLogIn(sender: LoginViewController, userJID: String, userPassword: String) {
         //Test
         print("got here1")
@@ -110,7 +103,6 @@ extension HomeTabBarController: LoginViewControllerDelegate {
         //Test
         print("got here2")
         do {
-            count = 3
             try self.xmppController = XMPPController(userJIDString: userJID,
                                                      password: userPassword)
             print("where am i?")
@@ -125,6 +117,7 @@ extension HomeTabBarController: LoginViewControllerDelegate {
         loggedIn = true
         NotificationCenter.default.post(name: .loggedIn, object: nil)
     }
+    */
 }
 
 extension HomeTabBarController: XMPPStreamDelegate {
